f6176d5e1db0b5f1637c1f9199d2eafa4b1d4102
ivangabriel.santos27 <ivangabriel.santos27@gmail.com>
2025-11-26 17:17:32 +0800

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app

---
+++ apps/my-microfrontend-app/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@my-microfrontend-app/my-microfrontend-app" />
+    </div>
+  );
+}
+
+export default App;
